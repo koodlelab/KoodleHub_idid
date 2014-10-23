eee2310c82857bb647827586c68b2808aa9b9723
koodle noodle <jingchen98@yahoo.com>
2014-10-23 19:03:52 +0800

    fix test

--- test/integration/signon_test.rb
+++ test/integration/signon_test.rb
@@ -15,7 +15,7 @@ class SignonTest < ActionDispatch::IntegrationTest
   test "valid signon" do
     get signup_path
     assert_difference 'User.count', 1 do
-      post users_path, user: {name: "koodle noodle", email:"koodlela@koodlelab.com",
+      post_via_redirect users_path, user: {name: "koodle noodle", email:"koodlela@koodlelab.com",
                               password:"temp1234", password_confirmation:"temp1234"}
     end
     assert_template 'users/show'
